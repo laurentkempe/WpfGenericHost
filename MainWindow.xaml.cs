6f6357956998621daca68ae26500e929220cf389
Laurent Kempé <laurent.kempe@techheadbrothers.com>
2023-11-16 22:17:05 +0100

    Refactor MainWindow.xaml.cs indentation and cleanup
    The commit consolidates and corrects the indentation within the MainWindow.xaml.cs file for better readability. It also removes unnecessary import of System.Windows namespace.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,16 +1,14 @@
-﻿using System.Windows;
-
-namespace wpfGenericHost;
+﻿namespace wpfGenericHost;
 
 /// <summary>
-/// Interaction logic for MainWindow.xaml
+///     Interaction logic for MainWindow.xaml
 /// </summary>
 public sealed partial class MainWindow
 {
     public MainWindow(ITextService textService)
     {
-            InitializeComponent();
+        InitializeComponent();
 
-            Label.Content = textService.GetText();
-        }
+        Label.Content = textService.GetText();
+    }
 }
